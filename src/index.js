c782dbab8e5c6525cc449d7882d2a34938f91d3b
Uenderss <meajudabarto@gmail.com>
2022-07-25 16:28:55 -0300

    primeira correcao  da versao final

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import express from "express";
 import cors from "cors";
-import dotenv from  "dotenv";
-
+import "dotenv/config";
 import routers from "./routes/index.js";
 
-dotenv config();
+
 
 const app=express();
 app.use(express.json());
 app.use(cors());
-app.use(router)
+app.use(routers)
 
 const port= process.env.PORT || 4000;
 app.listen(port,()=>{
